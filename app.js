ed4d5d5331e4c0fda16b9159df13c95ec1f919e2
flyingtext <flyingtext@nate.com>
2021-12-28 09:51:41 +0900

    deploying

--- app.js
+++ app.js
@@ -189,7 +189,7 @@ function app() {
               const prescpHanja = prescp.split('/')[0];
               const prescpHangul = prescp.split('/')[1];
               const prescpFrom = prescp.split('/')[2];
-              const prescpPage = parseInt(prescp.split('/')[3]);
+              const prescpPage = prescp.split('/')[3];
               console.log(prescp.split('/')[3]);
               let stmt;
               if(prescp.split('/')[3] == '(미기재)') {
